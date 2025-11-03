6bba9fecb7a51a2146114e18c693358e452f664d
enes.albas <albasenes35@gmail.com>
2025-11-03 15:07:54 +0300

    fourth commit

--- src/Main.java
+++ src/Main.java
@@ -16,4 +16,19 @@ public class Main {
             System.out.println("Giriş Başarısız");
         }
     }
+
+    public static void sum(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("First Number: ");
+        int firstNum = input.nextLine();
+        System.out.println("Second Number: ");
+        int secondNum = input.nextLine();
+        int sum = firstNum + secondNum;
+        System.out.println("Sum "+ sum);
+        int mult = firstNum + secondNum;
+        System.out.println("mult "+ mult);
+        if (mult > 5000 && sum < 4000){
+            System.out.println("Koşul sağlandı" + mult);
+        }
+    }
 }
