3d95d1a10137a8d11707e1326e1563a94cb29754
enes.albas <albasenes35@gmail.com>
2025-11-03 16:06:16 +0300

    updated commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,34 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Username: ");
+        String name = input.nextLine();
+
+        System.out.println("Password: ");
+        String password = input.nextLine();
+
+        if("admin".equalsIgnoreCase(name) && "1234".equals(password)){
+            System.out.println("Giriş Başarılı");
+        }else {
+            System.out.println("Giriş Başarısız");
+        }
+    }
+
+    public static void sum(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("First Number: ");
+        int firstNum = input.nextInt();
+        System.out.println("Second Number: ");
+        int secondNum = input.nextInt();
+        int sum = firstNum + secondNum;
+        System.out.println("Sum "+ sum);
+        int mult = firstNum * secondNum;
+        System.out.println("mult "+ mult);
+        if (mult > 5000 && sum < 4000){
+            System.out.println("Koşul sağlandı" + mult);
+        }
     }
 }
